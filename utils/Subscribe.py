893e17b6396f44ea981701faf866265ef8699e7e
GamerNoTitle <admin@bili33.top>
2022-02-17 16:12:48 +0800

    Edit: When the status_code is not 200, it will return the error

--- utils/Subscribe.py
+++ utils/Subscribe.py
@@ -1,11 +1,29 @@
 import requests as r
 import json as js
+import base64
 
-def subscribe(url):
+def subscribe(url, name):
     """
     Get the data of subscription
     """
-    return r.get(url).text
+    res = r.get(url)
+    if res.status_code == 200:
+        return res.text
+    else:
+        NoteConf = {
+            "v": "2",
+            "ps": "sub {} is unreachable with code {}".format(name, res.status_code),
+            "add": "google.com",
+            "port": "443",
+            "id": "2661b5f8-8062-34a5-9371-a44313a75b6b",
+            "aid": "2",
+            "net": "tcp",
+            "type": "none",
+            "host": "",
+            "tls": ""
+        }
+        VmessLink = 'vmess://' + base64.b64encode(str(NoteConf).replace('\n',''))
+        return base64.b64encode(str(VmessLink).replace('\n',''))
 
 if __name__ == '__main__':
     with open('../config.json','rt') as f:
@@ -19,7 +37,7 @@ if __name__ == '__main__':
             subls.append((subs[sub]['nick'],subs[sub]['link']))
     SubData = ''
     for nick,url in subls:
-        SubData += subscribe(url)
+        SubData += subscribe(url, nick)
     with open('../public/sub', 'wt') as f:
         f.write(SubData)
         f.close()
