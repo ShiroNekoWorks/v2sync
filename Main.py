c2c68b507a19096fc8f846eddde163a2d8d63dbf
GamerNoTitle <admin@bili33.top>
2022-02-18 00:38:28 +0800

    Alpha debug

--- Main.py
+++ Main.py
@@ -4,15 +4,17 @@ import os
 import shutil
 import base64
 
+
 def clean_public():
     '''
     Clean the public folder before the rendering process
     '''
     shutil.rmtree('./public')
     os.mkdir('public')
-    with open('./public/.gitkeep','wb') as f:
+    with open('./public/.gitkeep', 'wb') as f:
         f.writelines('')
         f.close()
+
 
 def read_config(file):
     '''
@@ -22,61 +24,79 @@ def read_config(file):
         config = js.load(json_file)
     return config
 
+
 def create_dictionary(path):
     '''
     Create a dictionary from the subscription file
     '''
     os.mkdir(path)
 
+
 if __name__ == "__main__":
+    print('[INFO]Initalizing...')
     clean_public()
+    print('[INFO]Initalization Completed!')
     # Create the folder from the path
-    conf=read_config('config.json')
-    global_conf=conf['global']
-    pathlist=global_conf['path'].split('/')
-    parent_path='./public'
+    print('[INFO]Creating the folder...')
+    conf = read_config('config.json')
+    global_conf = conf['global']
+    pathlist = global_conf['path'].split('/')
+    parent_path = './public'
     for i in pathlist:
-        if(i==pathlist[len(pathlist)-1]):
+        if(i == pathlist[len(pathlist)-1]):
             break
         create_dictionary(parent_path+'/'+i)
-        parent_path=parent_path+'/'+i
+        parent_path = parent_path+'/'+i
     # Creation completed
-    filename=pathlist[len(pathlist)-1]
+    print('[INFO]Created!')
+    filename = pathlist[len(pathlist)-1]
 
     # Start getting the data from the subscriptions
-    subs=conf['subs']
+    print('[INFO]Getting the data from the subscriptions...')
+    subs = conf['subs']
     SubList = []
     for sub in subs:
-        if subs[sub]['type']=='sub':
-            SubInfo=subscribe(subs[sub]['link'],subs[sub]['nick'])
+        if subs[sub]['type'] == 'sub':
+            print('[INFO]Getting the data from the subscription: {}'.format(
+                subs[sub]['nick']))
+            SubInfo = subscribe(subs[sub]['link'], subs[sub]['nick'])
+            print('[INFO]Got the data from the subscription: {}'.format(
+                subs[sub]['nick']))
+            print('[INFO]Saving the data to the temp: {}'.format(
+                subs[sub]['nick']))
             SubList.append(SubInfo)
+            print('[INFO]Saved the data to the temp: {}'.format(subs[sub]['nick']))
     # End getting the data from the subscriptions
 
     # Base64 decoding to get the vmess link
+    print('[INFO]Base64 decoding to get the vmess link...')
     DecodedList = []
     for i in SubList:
-        if SubList.index(i) == len(SubList) - 1:
-            Decoded = base64.b64decode(i).decode()  # The last one should not contain a ENTER
-        else:
-            Decoded = base64.b64decode(i).decode() + '\n'
+        Decoded = base64.b64decode(i).decode()
         DecodedList.append(Decoded)
+    print('[INFO]Base64 decoding completed!')
     # End base64 decoding
-    
+
     # Start combine all the links to a string for encoding
+    print('[INFO]Combining all the links to a string for encoding...')
     Combined = ''
     for i in DecodedList:
-        Combined += i
+        Combined += i.strip()
+    print('[INFO]Combined!')
     # End combine all the links to a string for encoding
 
     # Start encoding the string to formal vmess subscription format
+    print('[INFO]Encoding the string to formal vmess subscription format...')
     Encoded = base64.b64encode(Combined.encode())
+    print('[INFO]Encoded!')
     # End encoding the string to formal vmess subscription format
 
     # Start writing the data to the file
-    with open(conf['global']['path'], 'wb') as f:
+    print('[INFO]Writing the data to the file...')
+    with open('./public/' + conf['global']['path'], 'wb+') as f:
         f.write(Encoded)
         f.close()
+    print('[INFO]Writing completed!')
     # End writing the data to the file
 
     print('Done!')
-
--- utils/Subscribe.py
+++ utils/Subscribe.py
@@ -1,15 +1,39 @@
 import requests as r
+from requests import ConnectionError
 import json as js
 import base64
+
 
 def subscribe(url, name):
     """
     Get the data of subscription
     """
-    res = r.get(url)
+    try:
+        res = r.get(url, timeout=30)
+    except ConnectionError:
+        print('[ERROR]Connection timeout for {}'.format(name))
+        # NoteConf = {
+        #     "v": "2",
+        #     "ps": "sub {} is unavaliable because of network timeout".format(name, res.status_code),
+        #     "add": "google.com",
+        #     "port": "443",
+        #     "id": "2661b5f8-8062-34a5-9371-a44313a75b6b",
+        #     "aid": "2",
+        #     "net": "tcp",
+        #     "type": "none",
+        #     "host": "",
+        #     "tls": ""
+        # }
+        # print(js.dumps(str(NoteConf)))
+        # VmessLink = 'vmess://' + str(base64.b64encode(js.dumps(str(NoteConf).replace(
+        #     '\n', '').replace('b\'', '').replace('\'', '')).encode()))
+        # return base64.b64encode(str(VmessLink).replace('\n', '').replace('b\'', '').replace('\'', '').encode())
+        VmessLink = 'vmess://' + 'eyJ2IjoiMiIsInBzIjoiVGhpcyBzdWIgaXMgdW5hdmFsaWFibGUsIHBsZWFzZSBjaGVjayBpdCBvdXQhIiwiYWRkIjoid3d3LmcwMGdsZS5jb20iLCJwb3J0IjoiMTAwODYiLCJpZCI6IjI2MTIzM2Y4LTgwNjItMzRhNS05MzcxLWE0NDMzMzMzMzM2YiIsImFpZCI6IjIiLCJuZXQiOiJ0Y3AiLCJ0eXBlIjoibm9uZSIsImhvc3QiOiIiLCJ0bHMiOiIifQ==\n'
+        return base64.b64encode(str(VmessLink).encode())
     if res.status_code == 200:
         return res.text
     else:
+        print('[WARN]The subscription {} is not available, with the status code {}'.format(name, res.status_code))
         NoteConf = {
             "v": "2",
             "ps": "sub {} is unreachable with code {}".format(name, res.status_code),
@@ -22,14 +46,16 @@ def subscribe(url, name):
             "host": "",
             "tls": ""
         }
-        VmessLink = 'vmess://' + base64.b64encode(str(NoteConf).replace('\n',''))
-        return base64.b64encode(str(VmessLink).replace('\n',''))
+        VmessLink = 'vmess://' + str(base64.b64encode(str(NoteConf).replace(
+            '\n', '').replace('b\'', '').replace('\'', '').encode()))
+        return base64.b64encode(str(VmessLink).replace('\n', '').replace('b\'', '').replace('\'', '').encode())
+
 
 if __name__ == '__main__':
     '''
     Code for debug, also for single subscription
     '''
-    with open('../config.json','rt') as f:
+    with open('../config.json', 'rt') as f:
         config = f.read()
         f.close()
     config = js.loads(config)
@@ -37,9 +63,9 @@ if __name__ == '__main__':
     subls = []
     for sub in subs:
         if subs[sub]['type'] == 'sub':
-            subls.append((subs[sub]['nick'],subs[sub]['link']))
+            subls.append((subs[sub]['nick'], subs[sub]['link']))
     SubData = ''
-    for nick,url in subls:
+    for nick, url in subls:
         SubData += subscribe(url, nick)
     with open('../public/sub', 'wt') as f:
         f.write(SubData)
