7582f77ba5652774cf45c95e8245de3481c3668f
v2sync-org <v2sync@v2sync.org>
2022-02-17 09:45:34 +0800

    feat: subscription part

---
+++ utils/Subscribe.py
@@ -0,0 +1,25 @@
+import requests as r
+import json as js
+
+def subscribe(url):
+    """
+    Get the data of subscription
+    """
+    return r.get(url).text
+
+if __name__ == '__main__':
+    with open('../config.json','rt') as f:
+        config = f.read()
+        f.close()
+    config = js.loads(config)
+    subs = config['subs']
+    subls = []
+    for sub in subs:
+        if sub['type'] == 'sub':
+            subls.append((sub['nick'],sub['url']))
+    SubData = ''
+    for nick,url in subls:
+        SubData += subscribe(url)
+    with open('../public/sub', 'wt') as f:
+        f.write(SubData)
+        f.close()
